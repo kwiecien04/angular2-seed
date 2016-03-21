a5a4b156343bc307cb5f5002bb554790092e7ab2
Zibi Work <zbigniew.pytel@gmail.com>
2016-03-21 15:44:55 -0400

    hide the progerss

--- src/upload/progerss.component.ts
+++ src/upload/progerss.component.ts
@@ -3,23 +3,24 @@ import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector:'indicator-bar',
-    template:`<div class="progress">
+    template:`<div class="progress" [hidden]="!visible">
         <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow='progerss' aria-valuemin="0" aria-valuemax="100" style="width: {{progerss}}%;">
         <span class="sr-only">{{progerss}}% Complete</span>{{progerss}}%
     </div>
-    </div>`
+    </div>{{visible}}`
 })
 
 export class IndicatorBar implements OnInit{
     @Input() stream:Observable<number>;
+    @Input() visible:boolean;
     progerss:number=0;
     constructor(private ref:ChangeDetectorRef){
        
     }
     ngOnInit(){
-        console.log('im here');
+       
       setInterval(() => {
-      this.stream.subscribe((values:number)=>{  this.progerss = values;console.log(this.progerss);});
+      this.stream.subscribe((values:number)=>{this.progerss = values;this.visible=true;},(err)=>console.log(err),()=>{console.log('end');});
       this.ref.markForCheck();
     }, 1000);
     }
--- src/upload/uploadwithprogress.ts
+++ src/upload/uploadwithprogress.ts
@@ -7,28 +7,40 @@ import {IndicatorBar} from './progerss.component';
 @Component({
      selector:'upload-progerss',
     template:`
-    <h1>you are in upload progress</h1>
-    <input type="file" (change)="fileChangeEvent($event)" placeholder="Upload file..." />
-    <button type="button" (click)="upload()">Upload</button>
+  <h1>you are in upload progress</h1>
+     <!-- <input type="file" (change)="fileChangeEvent($event)" placeholder="Upload file..." />
+    <button type="button" (click)="upload()">Upload</button>-->
     
-    <label class="btn btn-primary" for="my-file-selector">
-    <input id="my-file-selector" type="file" style="display:none;">
-    Button Text Here
-     </label>
+   
+            <div class="input-group">
+                <span class="input-group-btn">
+                 <button type="button" [disabled]="!filename" (click)="upload()" ng class="btn btn-danger">Upload</button>
+                    <span class="btn btn-primary btn-file">
+                        Browse&hellip; <input type="file" (change)="fileChangeEvent($event)" multiple>
+                    </span>
+                </span>
+                <input type="text"  class="form-control" [ngModel]="filename" placeholder="please Browse to select file to upload" readonly>
+      </div>
+    
+    
+     
      <!--<div class="progress">
      <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow='ups' aria-valuemin="0" aria-valuemax="100" style="width: {{ups}}%;">
     <span class="sr-only">{{ups}}% Complete</span>{{ups}}%
   </div>
 </div>-->
-<indicator-bar [stream]=in></indicator-bar>
-     `,
+<indicator-bar [stream]=in [visible]=visible></indicator-bar>
+    here:{{visible}} `,
+    styleUrls:['./upload/upload.css'],
     providers:[FileUploadService],
     directives:[IndicatorBar]
 })
 export class UploadProgerss implements OnInit{
     in:Observable<number>;
     filesToUpload:Array<File>;
+    filename:string='';
     public ups:number=0;
+    visible:boolean=false;
     constructor(private _uploadService:FileUploadService,private ref:ChangeDetectorRef)  {
        setInterval(() => {
       _uploadService.getObserver().subscribe((progerss:number)=>{  this.ups = progerss;});
@@ -45,16 +57,26 @@ export class UploadProgerss implements OnInit{
     }
     upload(){
         this._uploadService.upload('http://localhost:3000/upload', this.filesToUpload).then((result) => {
-            console.log(result);
+            console.log('good');
+            setTimeout(()=>{this.visible=false;alert('zibi');},2000);
+            
         }, (error) => {
             console.error(error);
         });  
         
-    }
+    } 
         
     
     fileChangeEvent(fileInput:any){
         this.filesToUpload = <Array<File>>fileInput.target.files;
+        
+        var x:number;
+        for(x=0;x<this.filesToUpload.length;x++){
+            this.filename+=this.filesToUpload[x].name+ ' ';
+        }
+        
+       // this.filesToUpload.forEach((file)=>{this.filename+=file.name;});
+       // this.filename=this.filesToUpload[0].name;
     }
     
 }
